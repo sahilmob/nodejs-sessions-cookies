0696b2834f5db20dedf996efdb104cd9ea745049
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-11 14:59:29 +0200

    user user sotred in session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,20 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
 	const isLoggedIn = req.get("Cookie");
 	res.render("auth/login", {
 		path: "/login",
 		pageTitle: "Login",
-		isAuthenticated: req.isLoggedIn
+		isAuthenticated: req.session.isLoggedIn
 	});
 };
 
 exports.postLogin = (req, res, next) => {
-	req.session.isLoggedIn = true;
-	res.redirect("/");
+	User.findById("5be6dc02acfe2821a87bd7e2")
+		.then(user => {
+			req.session.isLoggedIn = true;
+			req.session.user = user;
+			res.redirect("/");
+		})
+		.catch(err => console.log(err));
 };
